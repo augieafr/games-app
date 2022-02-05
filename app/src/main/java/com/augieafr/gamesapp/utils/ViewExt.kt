e6a5312c000b5ae58f5b39f08803858ddd05da23
augieafr <augie.afriyansyah@kumparan.com>
2022-02-05 16:06:24 +0700

    feature(utils): view visible or gone

--- app/src/main/java/com/augieafr/gamesapp/utils/ViewExt.kt
+++ app/src/main/java/com/augieafr/gamesapp/utils/ViewExt.kt
@@ -5,3 +5,11 @@ import androidx.viewbinding.ViewBinding
 
 fun <T : ViewBinding> viewBindingWithBinder(binder: (View) -> T) =
     FragmentAutoClearedBinding(binder)
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
